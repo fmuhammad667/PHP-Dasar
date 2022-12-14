d3773c23f57dc042309d30a9fc0c671d8d5152d0
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-14 13:08:28 +0700

    add file 9.operator-aritmatika.php

---
+++ 9.operator-aritmatika.php
@@ -0,0 +1,35 @@
+<?php
+
+$a = 10;
+$b = 10;
+
+// pertambahan
+$hasil = $a + $b;
+var_dump($hasil);
+
+// konversi ke negatif
+$konversiNegative = -$hasil;
+var_dump($konversiNegative);
+
+// konversi negatif ke positif
+$konversiPositif = +$hasil;
+var_dump($konversiPositif);
+
+// modulo
+$modulo = 100 % 4;
+var_dump($modulo);
+
+// perkalian
+$perkalian = $a * 5;
+var_dump($perkalian);
+
+// pengurangan
+$pengurangan = $a - $b - 50;
+var_dump($pengurangan);
+
+// pembagian
+$pembagian = 100 / 5;
+var_dump($pembagian);
+
+$pangkat = 5 ** 5;
+var_dump($pangkat);
