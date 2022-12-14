8ee68defea18d44b66be47a2091dd6533255b851
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-14 07:39:04 +0700

    add file 5.variable.php

---
+++ 5.variable.php
@@ -0,0 +1,40 @@
+<?php
+
+$nama = "Fadhil";
+$age = 20;
+
+echo "Nama : ";
+echo $nama;
+echo "\n";
+
+echo "Umur : ";
+echo $age;
+echo "\n";
+
+// varialbe values
+$contoh = "vv"; // singkatan dari variable values
+$$contoh = "Fadhil"; // ini sama saja seperti $vv = "Fadhil";
+
+echo "value dari variable Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "value dari variable values contoh : ";
+echo $$contoh;
+echo "\n";
+
+// atau kita juga bisa menggantinya
+echo $vv;
+echo "\n";
+
+
+
+
+
+
+
+
+
+
+
+
