56520e9b3ffe6bbbcc1970aa8ec088207382b4e4
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-14 10:40:17 +0700

    add file 6.constant.php

---
+++ 6.constant.php
@@ -0,0 +1,15 @@
+<?php
+
+// deklarasi sebuah constant
+define("AUTHOR", "Fadhil Muhammad");
+define("APP_VERSION", 0.1);
+
+echo "Author : ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version : ";
+echo APP_VERSION;
+echo "\n";
+
+
