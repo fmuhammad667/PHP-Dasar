4bc66e83eedfe560d3c9bb173963c2e6f278fb5a
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-14 16:59:05 +0700

    add file 12.operator-logika.php

---
+++ 12.operator-logika.php
@@ -0,0 +1,19 @@
+<?php
+
+// operasi logika
+
+// And (&&)
+var_dump(true && true); // (true)
+var_dump(true aNd false); // (false)
+
+// Or (||)
+var_dump(false || true); // (true)
+var_dump(true OR true); // (true)
+
+// Xor 
+var_dump(true xor false); // (true)
+var_dump(true xor true); // (false)
+
+// Not (!)
+var_dump(!true); // (false)
+var_dump(!false); // (true)
