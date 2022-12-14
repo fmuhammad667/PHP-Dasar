47ae8351876d4a750d387c2c2d78b62747f8fe3e
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-14 13:42:57 +0700

    add file 11.operator-penugasan.php

---
+++ 11.operator-perbandingan.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump(10 == "10"); // sama dengan (true)
+var_dump(10 === "10"); // sama dengan identik (false)
+var_dump(10 != "10"); // tidak sama dengan (false)
+var_dump(10 !== "10"); // tidak sama dengan identik (true)
+var_dump(10 < 9); // kurang dari (false)
+var_dump(10 <= 9); // kurang dari atau sama dengan (false)
+var_dump(9 >= 9); // lebih dari atau sama dengan (true)
+var_dump(9 > 7); // lebih dari (true)
