8aad62aaa7462a9cc9af47348fc8402061e3fd8b
fmuhammad667 <fadhilmuh667@gmail.com>
2022-12-15 16:10:39 +0700

    add file 22.do-while-loop.php

---
+++ 22.do-while-loop.php
@@ -0,0 +1,7 @@
+<?php
+
+$counter = 1;
+do {
+    echo "ini adalah perulangan do while ke-$counter" . PHP_EOL;
+    $counter++;
+} while ($counter <= 10); // untuk do while jika kondisi bernilai false maka akan dilakukan perulangan minimal satu kali
